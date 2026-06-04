92beda0be7ac997ff9b95fee1b2df8229bc18e1c
Mrfaheem4 <mrfaheem010@gmail.com>
2026-06-04 21:05:56 +0500

    Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { CanvasProvider } from "./context/CanvasContext";
 import WhiteboardPage from "./components/WhiteboardPage";
+import LoginCard from "./pages/LoginCard";
 
 function App() {
-  return (
-    <CanvasProvider>
-      <div className="w-screen h-screen flex flex-col">
-        <WhiteboardPage />
-      </div>
-    </CanvasProvider>
-  );
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isAuthenticated) {
+    return (
+      <CanvasProvider>
+        <div className="w-screen h-screen flex flex-col">
+          <WhiteboardPage />
+        </div>
+      </CanvasProvider>
+    );
+  }
+
+  return <LoginCard />;
 }
 
 export default App;
